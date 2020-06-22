00d2065e4c67b23d339513ea933a0ef5422990fb
hansik <imaec@t-monet.com>
2020-06-22 14:29:02 +0900

    - init commit2

--- library/src/main/java/com/imaec/mvvmhelper/base/BaseAdapter.kt
+++ library/src/main/java/com/imaec/mvvmhelper/base/BaseAdapter.kt
@@ -1,4 +1,26 @@
 package com.imaec.mvvmhelper.base
 
-class BaseAdapter {
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    protected val TAG = this::class.java.simpleName
+    protected val listItem = ArrayList<Any>()
+
+    protected lateinit var binding: ViewDataBinding
+
+    override fun getItemCount(): Int = listItem.size
+
+    fun addItem(item: Any) {
+        listItem.add(item)
+    }
+
+    fun addItems(list: ArrayList<Any>) {
+        listItem.addAll(list)
+    }
+
+    fun clearItem() {
+        listItem.clear()
+    }
 }
