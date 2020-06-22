d4145f15a67dab053fccaf29c78e1e1fc856f36e
imaec <dev.imaec@gmail.com>
2020-06-22 16:42:43 +0900

    - BaseViewHolder 수정

--- app/src/main/java/com/imaec/mvvmhelpersample/MainAdapter.kt
+++ app/src/main/java/com/imaec/mvvmhelpersample/MainAdapter.kt
@@ -1,26 +1,29 @@
 package com.imaec.mvvmhelpersample
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.imaec.mvvmhelper.base.BaseAdapter
-import com.imaec.mvvmhelper.base.BaseViewHolder
-import com.imaec.mvvmhelpersample.databinding.ItemMainBinding
-
-class MainAdapter : BaseAdapter() {
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        binding = ItemMainBinding.inflate(LayoutInflater.from(parent.context))
-        return ItemViewHolder(binding as ItemMainBinding)
-    }
-
-    inner class ItemViewHolder(binding: ItemMainBinding) : BaseViewHolder(binding) {
-
-        override fun onBind(item: Any) {
-
-        }
-    }
-}
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.RecyclerView
+//import com.imaec.mvvmhelper.base.BaseAdapter
+//import com.imaec.mvvmhelper.base.BaseViewHolder
+//import com.imaec.mvvmhelpersample.databinding.ItemMainBinding
+//
+//class MainAdapter : BaseAdapter() {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        binding = ItemMainBinding.inflate(LayoutInflater.from(parent.context))
+//        return ItemViewHolder(binding as ItemMainBinding)
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//        if (holder is ItemViewHolder) {
+//            holder.onBind(listItem[position] as String)
+//        }
+//    }
+//
+//    inner class ItemViewHolder(binding: ItemMainBinding) : BaseViewHolder<String>(binding) {
+//
+//        override fun onBind(item: String) {
+//            super.onBind(item)
+//        }
+//    }
+//}
--- library/src/main/java/com/imaec/mvvmhelper/base/BaseViewHolder.kt
+++ library/src/main/java/com/imaec/mvvmhelper/base/BaseViewHolder.kt
@@ -3,13 +3,13 @@ package com.imaec.mvvmhelper.base
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseViewHolder(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+abstract class BaseViewHolder<T>(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
 
     open fun onBind() {
 
     }
 
-    open fun onBind(item: Any) {
+    open fun onBind(item: T) {
 
     }
 }
